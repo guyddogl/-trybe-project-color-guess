70fd027ab2e874e5cead589a7594aa4890f683e9
Guyddo <guyddogl@gmail.com>
2022-05-30 19:18:15 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 const sectionColorGuess = document.getElementById('color-guess');
 const pColorGuess = document.getElementById('rgb-color');
+const pAnswer = document.getElementById('answer');
 
 // Requisito 4
 function generateColors() {
   const arrayColor = [];
   for (let index = 0; index < 3; index += 1) {
-    const numberColor = Math.floor(Math.random() * 255);
+    const numberColor = Math.floor(Math.random() * 256);
     arrayColor[index] = numberColor;
   }
-  const randomColor = `rgb(${arrayColor[0]} , ${arrayColor[1]} , ${arrayColor[2]})`;
+  const randomColor = `rgb(${arrayColor[0]}, ${arrayColor[1]}, ${arrayColor[2]})`;
   return randomColor;
+}
+
+// Requisito 5
+function rightOrWorng(event) {
+  const clickedColor = event.target.style.backgroundColor;
+  if (clickedColor === pColorGuess.innerText) {
+    pAnswer.innerText = 'Acertou!';
+  } else {
+    pAnswer.innerText = 'Errou! Tente novamente!';
+  }
 }
 
 // Requisito 3
@@ -21,17 +32,12 @@ function generateCircles() {
     const coloredCircles = document.createElement('div');
     coloredCircles.classList.add('ball');
     coloredCircles.style.backgroundColor = newColor;
+    coloredCircles.addEventListener('click', rightOrWorng)
     sectionColorGuess.appendChild(coloredCircles);
   }
   let randomNumber = Math.floor(Math.random() * arrayColors.length);
   trueColor = arrayColors[randomNumber];
-  pColorGuess.innerText = trueColor;
-  // console.log(trueColor);
+  pColorGuess.innerText = trueColor; // Requisito 2
 }
 
 generateCircles();
-
-// Requisito 2
-function colorToGuess() {
-
-}
